194c5fb24d37e52486d46158f238b0a33da44517
Vicente <vicentermzchz@gmail.com>
2026-02-27 21:55:07 -0600

    Try catch add it, condition statements can't enter zero value

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,73 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        // Money borrow
-        System.out.print("Principal: ");
-        String principal = scanner.nextLine().trim();
-        float principalFloat = Float.parseFloat(principal);
+
+        // User input strings
+        //String interestRate;
+        String period;
+
+        // conver user strings
+        int principalInt = 0;
+        float interestRateFloat = 0;
+        int periodInt = 0;
+
+        do {
+            // Loan moeny
+            try {
+                // Money borrow
+                System.out.print("Principal: ");
+                principalInt = Integer.parseInt(scanner.nextLine().trim());
+
+                if (principalInt == 0){
+                    System.out.println("(Principal) can't be zero");
+                } else if (principalInt < 2000) {
+                    System.out.println("Loan must be equal or higher to $2000");
+                }
+            }
+            catch (NumberFormatException e){
+                System.out.println("(Principal) is not a number");
+            }
+        } while (principalInt == 0 || principalInt < 2000);
 
         // interest
-        System.out.print("Annual interest Rate: ");
-        String interestRate = scanner.nextLine().trim();
-        float interestRateFloat = Float.parseFloat(interestRate);
+        do {
+            try {
+                System.out.print("Annual interest Rate: ");
+                interestRateFloat = Float.parseFloat(scanner.nextLine().trim());
 
-        //period
-        System.out.print("period: ");
-        String period = scanner.nextLine().trim();
-        int periodInt = Integer.parseInt(period);
+                if (interestRateFloat == 0){
+                    System.out.println("(interest) can't be zero");
+                } else if (interestRateFloat < 2) {
+                    System.out.println("interest must be equal or higher to 2.0");
+                }
+            }
+            catch (NumberFormatException e){
+                System.out.println("(Principal) is not a number");
+            }
+
+        } while(interestRateFloat == 0 || interestRateFloat < 2);
+
+
+        do {
+
+            //period
+            System.out.print("period: ");
+            period = scanner.nextLine().trim();
+
+        } while (period.equals(""));
+
+
+
+        periodInt = Integer.parseInt(period);
+
+
 
         // Interes
         int month = periodInt * 12;
         float totalInterest = ((interestRateFloat / 100)/12);
         float totalInterest2 = (float)Math.pow(1 + totalInterest, month) * totalInterest;
         float totalInterest3 = (float)Math.pow(1 + totalInterest, month) - 1;
-        float total = principalFloat * (totalInterest2 / totalInterest3);
+        float total = principalInt * (totalInterest2 / totalInterest3);
 
         // Print out to user
         NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
